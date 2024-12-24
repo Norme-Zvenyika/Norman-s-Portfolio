7d63612a1ceabbbc127a4cc976657a46cb8c098d
noz224 <noz224@lehigh.edu>
2024-12-24 08:33:18 -0500

    add a function to run a query on the database

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,13 +1,32 @@
-import postgres from 'postgres';
+import postgres from "postgres";
 import "./env.config.js";
 
-
-// supabase postgres connection configuration
-export const db = postgres(process.env.DATABASE_URL, {
+// create the postgres instance
+const db = postgres(process.env.DATABASE_URL, {
     idle_timeout: 20
 });
 
 // cleanup function for tests/server shutdown
-export const closeConnection = async () => {
+const closeConnection = async () => {
     await db.end();
 };
+
+/**
+ * executes a sql query with placeholders
+ * @param {object} queryObject - the query object
+ * @param {string} queryObject.sql - the sql query text
+ * @param {any[]} queryObject.params - the query parameters
+ * @returns {Promise<any[]>} - the query result (rows)
+ */
+const query = async (queryObject) => {
+    try {
+        const { sql, params } = queryObject;
+        const rows = await db.unsafe(sql, params);
+        return rows;
+    } catch (err) {
+        console.error("problem executing query:", err);
+        throw err;
+    }
+};
+
+export { db, closeConnection, query };
