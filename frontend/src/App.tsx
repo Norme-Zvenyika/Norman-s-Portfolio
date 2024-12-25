d8c9c07b5dc518ea7280d57bf21b0e106a84b081
noz224 <noz224@lehigh.edu>
2024-12-25 06:07:04 -0500

    add the structure of the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-function App() {
+import { Box } from '@mui/material'
+import Sidebar from './components/Sidebar/Sidebar'
+import MainContent from './components/MainContent/MainContent'
 
+function App(): JSX.Element {
   return (
-    <div>
-      Portfolio Sites By Norman Zvenyika
-    </div>
-  ) 
+    <Box sx={{ 
+      display: 'flex',
+      flexDirection: {
+        xs: 'column',
+        md: 'row'     
+      }
+    }}>
+      <Sidebar />
+      <MainContent />
+    </Box>
+  )
 }
 
 export default App
