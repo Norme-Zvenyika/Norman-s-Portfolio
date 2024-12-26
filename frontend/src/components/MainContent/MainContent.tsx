2bf65b6dc2e6d71c5167ac7e57a80c0e0c6225d7
noz224 <noz224@lehigh.edu>
2024-12-26 06:04:29 -0500

    pass section data as aprop

--- frontend/src/components/MainContent/MainContent.tsx
+++ frontend/src/components/MainContent/MainContent.tsx
@@ -1,27 +1,31 @@
 import { Box, Divider } from '@mui/material'
 import { AboutSection } from '../Sections/AboutSection'
 import { Section } from '../../types/section'
+import aboutContent from '../../data/about.json'
 
 interface Props {
   sections: Section[]
 }
 
 const MainContent = ({ sections }: Props) => {
+
   return (
-    <Box sx={{
-      width: '100%',
-      scrollPaddingTop: {
-        xs: '70px',
-        md: 0
-      },
-      minHeight: '100vh'
-    }}>
-      {sections.map(({ id }, index) => (
-        <Box key={id}>
-          <AboutSection />
-          {index !== sections.length - 1 && (
-            <Divider />
+    <Box
+      sx={{
+        width: '100%',
+        scrollPaddingTop: {
+          xs: '70px',
+          md: 0,
+        },
+        minHeight: '100vh',
+      }}
+    >
+      {sections.map((section, index) => (
+        <Box key={section.id}>
+          {section.id === 'about' && (
+            <AboutSection section={section} content={aboutContent} />
           )}
+          {index !== sections.length - 1 && <Divider />}
         </Box>
       ))}
     </Box>
