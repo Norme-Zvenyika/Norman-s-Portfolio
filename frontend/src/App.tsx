23bf6cb87c9aaf4d845d4de86fbe00445114a528
noz224 <noz224@lehigh.edu>
2025-02-02 19:39:51 -0500

    ignore the formatting for now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,12 @@ import { ThemeProvider } from '@mui/material/styles'
 import { theme } from './theme/theme'
 
 function App(): JSX.Element {
-  return (
-    <ThemeProvider theme={theme}>
-      <AppLayout />
-    </ThemeProvider>
-  )
+  return (<AppLayout/>)
+  // return (
+  //   <ThemeProvider theme={theme}>
+  //     <AppLayout />
+  //   </ThemeProvider>
+  // )
 }
 
 export default App;
